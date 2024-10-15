84ebce998f0d57a02c8c412170cd4bc7e062ee3b
Mihu <363012115@qq.com>
2024-10-15 18:25:14 +0800

    Update test.js
    普通插件注释

--- js/test.js
+++ js/test.js
@@ -1,4 +1,16 @@
-
+/**
+curve(p0: cc.Vec2, p1, p2, p3) {
+        let s = 0.5;
+        let b1 = p0.mul(-s).add(p1.mul(2 - s)).add(p2.mul(s - 2)).add(p3.mul(s));
+        let b2 = p0.mul(2 * s).add(p1.mul(s - 3)).add(p2.mul(3 - 2 * s)).add(p3.mul(-s));
+        let b3 = p0.mul(-s).add(p2.mul(s));
+        let b4 = p1;
+        function fx(x) {
+            return b1.mul(Math.pow(x, 3)).add(b2.mul(Math.pow(x, 2))).add(b3.mul(x)).add(b4)
+        }
+        return fx;
+    }
+*/
 let Vec3 = require('./Vec3')
 let CatmullRom = require('./CatmullRom') 
 
